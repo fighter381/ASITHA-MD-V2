1e44527f2a23602484e38e5f0c1e7ac3a824c16b
fighter381 <xohode5401@outlook.com>
2024-11-23 10:35:31 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'PRABATH-MD~LXpUgLbK#4ob0git0LtbfYqGzewMVnxK0uHGwxdLauCizVHlutTc' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=u2QkWSKB#PA-RfVM57dsS22RBwFpe-kt3o1m9bGF7djBClnL9xKQ' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
